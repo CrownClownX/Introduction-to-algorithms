a04df2df7c4dbdc09be26de4b382957e7770b5a7
Mateusz Hamerla <hamerlamateusz@gmail.com>
2018-01-03 11:43:20 +0100

    Update 1. InsertionAlgorithm.cpp

--- 1. InsertionAlgorithm.cpp
+++ 1. InsertionAlgorithm.cpp
@@ -1,39 +1,54 @@
 #include <iostream>
 
-//tabQuantity is needed because sizeof(tab) can't get size of array in function
-
-void insertionSort(float tab[], const int& tabQuantity)
+template <class T>
+void insertionSort(T tab[], const int& tabQuantity)
 {
-    if(tabQuantity > 1)
+    for(int i = 1; i < tabQuantity; i++)
     {
-        float key = 0.0;
+        T key = tab[i];
+        int j = i - 1;
 
-        for(int i = 1; i < tabQuantity; i++)
+        while(j >= 0 && tab[j] > key)
         {
-            key = tab[i];
-            int j = i - 1;
-
-            while(j >= 0 && tab[j] > key)
-            {
-                tab[j+1] = tab[j];
-                j = j - 1;
-            }
-
-            tab[j+1] = key;
+            tab[j+1] = tab[j];
+            j--;
         }
+
+        tab[j+1] = key;
     }
-    else
-        std::cout << "Too few elements" << std::endl;
+}
+
+template <class T>
+void showTab(const T tab[], const int& tabQuantity)
+{
+    for(int i = 0; i < tabQuantity; i++)
+        std::cout << tab[i] << " ";
+
+    std::cout << std::endl;
 }
 
 int main()
 {
-    float tab[] = {3.3, 1.1, 22.3, 4.5, 88.7, 22.3, 55.2, 1.1};
-    insertionSort(tab, sizeof(tab)/sizeof(float));
+    float fTab[] = {12.32, 4.2, 2.1, 33.6, 6.2334, 776.654, 9.878, 6.04, 23.66, 455.12, 5.66, 3.54, 8.56, 0.1};
+    int fMax = sizeof(fTab)/sizeof(float);
 
-    if((sizeof(tab)/sizeof(float)) > 1)
-        for(int i = 0; i < sizeof(tab)/sizeof(float); i++)
-            std::cout << tab[i] << std::endl;
+    int iTab[] = {43, 2, 3, 23, 434, 66, 8, 88, 9, 1, 233};
+    int iMax = sizeof(iTab)/sizeof(int);
+
+    long lTab[] = {32323232, 4, 4334, 4567887, 88438, 2122, 374637437, 383848, 283293299, 2312312, 32, 1, 3, 4,5 ,43434, 322, 34435555};
+    int lMax = sizeof(lTab)/sizeof(long);
+
+    std::cout << "INT : " << std::endl;
+    insertionSort(iTab, iMax);
+    showTab(iTab, iMax);
+
+    std::cout << "LONG : " << std::endl;
+    insertionSort(lTab, lMax);
+    showTab(lTab, lMax);
+
+    std::cout << "FLOAT : " << std::endl;
+    insertionSort(fTab, fMax);
+    showTab(fTab, fMax);
 
     return 0;
 }
